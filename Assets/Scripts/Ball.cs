92a4689d24efea48199eb20e8c076f617c6a1404
Thannorh <hunter.powrie@digipen.edu>
2020-01-30 08:54:53 -0800

    Added Paddle + Ball Scripts :)

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    //Variables
+    public float Speed = 4.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().AddForce(Random.insideUnitCircle.normalized * Speed * 20);
+    }
+    void Update()
+    {
+
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "Paddle")
+        {
+            //Calculate hit Value
+            float hitValue = Ricochet(transform.position, collision.transform.position, collision.collider.bounds.size.x);
+            Vector2 Direction = new Vector2(hitValue, 1).normalized;
+
+            //Give the ball the direction necessary
+            GetComponent<Rigidbody2D>().velocity = Direction * Speed;
+        }
+        
+    }
+
+    //This function is supposed to replicate the ricochet used in Arkanoid
+    private float Ricochet(Vector2 Ball, Vector2 Paddle, float Position)
+    {
+        //this should return a certain value that we can use to ricochet properly
+        return (Ball.x - Paddle.x) / Position;
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,51 @@
+﻿/* 
+ Paddle.cs
+ Description: Paddle Movement
+ */ 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //Variables
+    public float Speed = 2.0f;
+    public float MaxLeft = -4.0f;
+    public float MaxRight = 4.0f;
+    // Main Function for movement
+    void Update()
+    {
+        //Check for input
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //Moves the paddle Right, stops if going too far
+            if (transform.position.x > MaxRight)
+            {
+                transform.position = new Vector3(MaxRight, transform.position.y, 0);
+            }
+            else
+            {
+                transform.position += Vector3.right * Speed;
+            }
+        }
+        //Check for input
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //Moves the paddle left, stops if going too far
+            if (transform.position.x < MaxLeft)
+            {
+                transform.position = new Vector3(MaxLeft, transform.position.y, 0);
+            }
+            else
+            {
+                transform.position += Vector3.left * Speed;
+            }
+        }
+    }
+}
