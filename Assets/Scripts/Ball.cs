514e656442998998ccbf46b08e40f8268b008c9d
Thannorh <hunter.powrie@digipen.edu>
2020-02-03 07:20:07 -0800

    Update: Modified Ball Scripts

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -10,7 +10,7 @@ public class Ball : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Rigidbody2D>().velocity = Vector2.up * Speed;
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * Speed / 2;
     }
     void Update()
     {
