5f5aff41be7277da1ab4ea4d33973d1ad314e750
Jacob <jacob.arnez@digipen.com>
2020-02-03 07:37:59 -0800

    Arkanoid added

---
+++ Arkanoid/Assets/Scripts/Block.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour
+{
+
+    void OnCollisionEnter2D(Collision2D collisionInfo)
+    {
+        // Destroy the whole Block
+        Destroy(gameObject);
+    }
+}
