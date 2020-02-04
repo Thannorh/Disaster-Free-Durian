33cf9e38767f5af865506bebf922ab00f4eef520
Jacob <jacob.arnez@digipen.com>
2020-02-04 09:39:56 -0800

    Added more to block script letting random drops

--- Arkanoid/Assets/Scripts/Block.cs
+++ Arkanoid/Assets/Scripts/Block.cs
@@ -5,11 +5,46 @@ using UnityEngine;
 using UnityEngine;
 using System.Collections;
 
+
+
 public class Block : MonoBehaviour
 {
+    public Transform Enlarge;
+
+    public int whichpowerup;
 
     void OnCollisionEnter2D(Collision2D collisionInfo)
     {
+
+        whichpowerup = Random.Range(1 , 6);
+
+        if (whichpowerup == 1)
+        {
+            Instantiate (Enlarge, transform.position, Enlarge.rotation);
+
+        }
+        if (whichpowerup == 2)
+        {
+            Instantiate(Enlarge, transform.position, Enlarge.rotation);
+
+        }
+        if (whichpowerup == 3)
+        {
+            Instantiate(Enlarge, transform.position, Enlarge.rotation);
+
+        }
+        if (whichpowerup == 4)
+        {
+            Instantiate(Enlarge, transform.position, Enlarge.rotation);
+
+        }
+        if (whichpowerup == 5)
+        {
+            Instantiate(Enlarge, transform.position, Enlarge.rotation);
+
+        }
+
+
         // Destroy the whole Block
         Destroy(gameObject);
     }
