5d806178a29bbb665d02267004fbbf884fe09548
Thannorh <hunter.powrie@digipen.edu>
2020-02-06 09:19:40 -0800

    Update

--- Correct Arkanoid/Arkanoid/Assets/Scripts/PowerUpCollision.cs
+++ Correct Arkanoid/Arkanoid/Assets/Scripts/PowerUpCollision.cs
@@ -19,6 +19,7 @@ public class PowerUpCollision : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Destroy(gameObject); 
+        
         if(collision.gameObject.name == "Slow")
         {
 
