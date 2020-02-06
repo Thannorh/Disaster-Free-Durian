e77bf64682e6e4bd66384e44aa75d6d19d439006
Jacob <jacob.arnez@digipen.com>
2020-02-06 07:37:17 -0800

    thew

--- Arkanoid/Assets/Scripts/Block.cs
+++ Arkanoid/Assets/Scripts/Block.cs
@@ -9,38 +9,51 @@ using System.Collections;
 
 public class Block : MonoBehaviour
 {
-    public Transform Enlarge;
+    public GameObject Enlarge;
+    public GameObject Laser;
+    public GameObject Duplicate;
+    public GameObject Catch;
+    public GameObject Slow;
+    public GameObject Player;
 
     public int whichpowerup;
 
     void OnCollisionEnter2D(Collision2D collisionInfo)
     {
 
-        whichpowerup = Random.Range(1 , 6);
-
-        if (whichpowerup == 1)
+        whichpowerup = Random.Range(1 , 11);
+        if (Enlarge != null)
         {
-            Instantiate (Enlarge, transform.position, Enlarge.rotation);
+            if (whichpowerup == 1)
+            {
+                Instantiate(Enlarge, transform.position, Enlarge.transform.rotation);
 
-        }
-        if (whichpowerup == 2)
-        {
-            Instantiate(Enlarge, transform.position, Enlarge.rotation);
+            }
+            if (whichpowerup == 2)
+            {
+                Instantiate(Laser, transform.position, Laser.transform.rotation);
 
-        }
-        if (whichpowerup == 3)
-        {
-            Instantiate(Enlarge, transform.position, Enlarge.rotation);
+            }
+            if (whichpowerup == 3)
+            {
+                Instantiate(Duplicate, transform.position, Duplicate.transform.rotation);
 
-        }
-        if (whichpowerup == 4)
-        {
-            Instantiate(Enlarge, transform.position, Enlarge.rotation);
+            }
+            if (whichpowerup == 4)
+            {
+                Instantiate(Catch, transform.position, Catch.transform.rotation);
 
-        }
-        if (whichpowerup == 5)
-        {
-            Instantiate(Enlarge, transform.position, Enlarge.rotation);
+            }
+            if (whichpowerup == 5)
+            {
+                Instantiate(Slow, transform.position, Slow.transform.rotation);
+
+            }
+            if (whichpowerup == 6)
+            {
+                Instantiate(Player, transform.position, Player.transform.rotation);
+
+            }
 
         }
 
