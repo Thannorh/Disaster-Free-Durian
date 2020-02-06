49fb64d8cb2015a7fb7a6fcbea1caebbfd50cd1e
Jacob <jacob.arnez@digipen.com>
2020-02-06 08:13:06 -0800

    The fix of the century

---
+++ Correct Arkanoid/Arkanoid/Assets/Scripts/Block.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+using System.Collections;
+
+
+
+public class Block : MonoBehaviour
+{
+    public GameObject Enlarge;
+    public GameObject Laser;
+    public GameObject Duplicate;
+    public GameObject Catch;
+    public GameObject Slow;
+    public GameObject Player;
+
+    public int whichpowerup;
+
+    void OnCollisionEnter2D(Collision2D collisionInfo)
+    {
+
+        whichpowerup = Random.Range(1 , 11);
+        if (Enlarge != null)
+        {
+            if (whichpowerup == 1)
+            {
+                Instantiate(Enlarge, transform.position, Enlarge.transform.rotation);
+
+            }
+            if (whichpowerup == 2)
+            {
+                Instantiate(Laser, transform.position, Laser.transform.rotation);
+
+            }
+            if (whichpowerup == 3)
+            {
+                Instantiate(Duplicate, transform.position, Duplicate.transform.rotation);
+
+            }
+            if (whichpowerup == 4)
+            {
+                Instantiate(Catch, transform.position, Catch.transform.rotation);
+
+            }
+            if (whichpowerup == 5)
+            {
+                Instantiate(Slow, transform.position, Slow.transform.rotation);
+
+            }
+            if (whichpowerup == 6)
+            {
+                Instantiate(Player, transform.position, Player.transform.rotation);
+
+            }
+
+        }
+
+
+        // Destroy the whole Block
+        Destroy(gameObject);
+    }
+}
---
+++ Correct Arkanoid/Arkanoid/Assets/Scripts/Duplicate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Duplicate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnCollisionEnter2D(Collision2D collisioninfo)
+    {
+      
+
+    }
+}
