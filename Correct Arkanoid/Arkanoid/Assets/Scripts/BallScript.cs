8d138a7d5509ec584500477aafc9d67fbfc25692
Thannorh <hunter.powrie@digipen.edu>
2020-02-11 09:43:55 -0800

    Final Push @ digipen

--- Correct Arkanoid/Arkanoid/Assets/Scripts/BallScript.cs
+++ Correct Arkanoid/Arkanoid/Assets/Scripts/BallScript.cs
@@ -18,7 +18,7 @@ public class BallScript : MonoBehaviour
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.name == "racket")
+        if (collision.gameObject.tag == "Ship")
         {
             //Calculate hit Value (aka, the x value we will need to change direction)
             float hitValue = Ricochet(transform.position, collision.transform.position, collision.collider.bounds.size.x);
